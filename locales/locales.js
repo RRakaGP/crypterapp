843d3ba055b4d476157f242dd4f63c312727ec72
RRakaGP <gpremsai2220@gmail.com>
2022-05-06 21:27:45 +0530

    commit on main

---
+++ locales/locales.js
@@ -0,0 +1,21 @@
+import en_US from "./en_US";
+import fr_FR from "./fr_FR";
+import de_DE from "./de_DE";
+import zh_CN from "./zh_CN";
+import ru_RU from "./ru_RU";
+import it_IT from "./it_IT";
+import sk_SK from "./sk_SK";
+import es_ES from "./es_ES";
+
+const locales = {
+  en_US,
+  fr_FR,
+  de_DE,
+  zh_CN,
+  ru_RU,
+  it_IT,
+  sk_SK,
+  es_ES
+};
+
+export default locales;
