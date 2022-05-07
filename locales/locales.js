a310994f2a20577d9c5cf83f5bc3c69d6da0fd4f
RRakaGP <gpremsai2220@gmail.com>
2022-05-07 09:07:46 +0530

    Merge branch 'main' of https://github.com/RRakaGP/crypterapp into main

--- locales/locales.js
+++ locales/locales.js
@@ -1,21 +1,13 @@
 import en_US from "./en_US";
 import fr_FR from "./fr_FR";
 import de_DE from "./de_DE";
-import zh_CN from "./zh_CN";
-import ru_RU from "./ru_RU";
 import it_IT from "./it_IT";
-import sk_SK from "./sk_SK";
-import es_ES from "./es_ES";
 
 const locales = {
   en_US,
   fr_FR,
   de_DE,
-  zh_CN,
-  ru_RU,
-  it_IT,
-  sk_SK,
-  es_ES
+  it_IT
 };
 
 export default locales;
